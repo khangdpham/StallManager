b59ef4722f16ce4d1a3b4c73c0dcb79d44791705
Khang Pham <kpham45@gatech.edu>
2015-03-07 16:57:44 -0800

    Update project

--- src/edu/gatech/seclass/Customer.java
+++ src/edu/gatech/seclass/Customer.java
@@ -2,8 +2,6 @@ package edu.gatech.seclass;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class Customer extends Activity {
 
@@ -13,22 +11,4 @@ public class Customer extends Activity {
 		setContentView(R.layout.activity_customer);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.customer, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
